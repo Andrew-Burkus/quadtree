0ee82c99f7ef422612afdad50c562aaecdea77e4
Andrew G Burkus <da8urk@gmail.com>
2015-08-12 14:37:23 -0700

    damnit regexp

--- src/region_quadtree.js
+++ src/region_quadtree.js
@@ -1,6 +1,6 @@
 class RegionQuadTree extends QuadTree {
     constructor(width, height, max_depth, max_objects) {
         super(width, height, max_depth, max_objects);
-        this.root = new RegionNode(0, 0, width, height, 0);
+        this.root = new RegionNode(0, 0, width, height, 0, max_depth, max_objects);
     }
 }
